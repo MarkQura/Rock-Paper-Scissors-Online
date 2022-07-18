5d55adf662866954ad5aefa72caa2823d945bd06
MarkQura <prision09@gmail.com>
2022-07-18 18:38:38 +0100

    Fixed bug in winner
    Function winner would display that
    player lost when chosen scissors and
    computer chose paper
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let getComputerChoice = () => Math.floor(Math.random() * 3) + 1;
 let  winner = (player, cpu) => {
     if (cpu == player) {
         return null
-    } else if ((player > cpu || (cpu == 3 && player == 1)) && (cpu != 1 && player != 3)) {
+    } else if ((player > cpu || (cpu == 3 && player == 1)) && (cpu != 1 || player != 3)) {
         return 1;
     } else {
         return 0;
@@ -58,5 +58,7 @@ let game = () => {
         (final[4] == `W`) ? playerWins += 1 : cpuWins += 1
     }
 }
+
+winner(3, 1)
 
 game()
